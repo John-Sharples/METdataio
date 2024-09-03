9cb90039bc9ca5d91b4a0aa56032ca6a6c63338d
John Sharples <john.sharples@bom.gov.au>
2024-09-03 20:50:44 +0000

    host test_db

--- METdbLoad/test/test_mysql_conn.py
+++ METdbLoad/test/test_mysql_conn.py
@@ -2,10 +2,12 @@ import pymysql
 
 
 def test_mysql_available():
-    conn = pymysql.connect(host='test_db',
+    conn = pymysql.connect(host='127.0.0.1',
                     port=3306,
                     user='root',
-                    password='root_password')
+                    password='root_password'
+                    database='test_db'
+                    )
     
     conn.cursor().execute("CREATE DATABASE mv_test")
     
