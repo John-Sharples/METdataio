f72f9d60ad59299bca63fee61dc2c44d456f1f34
John Sharples <john.sharples@bom.gov.au>
2024-09-03 20:43:16 +0000

    host test_db

--- METdbLoad/test/test_mysql_conn.py
+++ METdbLoad/test/test_mysql_conn.py
@@ -2,7 +2,7 @@ import pymysql
 
 
 def test_mysql_available():
-    conn = pymysql.connect(host='host.docker.internal',
+    conn = pymysql.connect(host='test_db',
                     port=3306,
                     user='root',
                     password='root_password')
