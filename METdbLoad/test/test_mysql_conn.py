9f6432a3c06ec005c2ef76219e9b79ce88026507
John Sharples <john.sharples@bom.gov.au>
2024-09-03 21:10:10 +0000

    host again

--- METdbLoad/test/test_mysql_conn.py
+++ METdbLoad/test/test_mysql_conn.py
@@ -2,7 +2,7 @@ import pymysql
 
 
 def test_mysql_available():
-    conn = pymysql.connect(host='mariadb',
+    conn = pymysql.connect(host='localhost',
                     port=3306,
                     user='root',
                     password='root_password',
