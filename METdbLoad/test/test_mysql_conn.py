119c5320308e2c37335a1cf7679570c476d991da
John Sharples <john.sharples@bom.gov.au>
2024-09-03 20:13:01 +0000

    fix host

--- METdbLoad/test/test_mysql_conn.py
+++ METdbLoad/test/test_mysql_conn.py
@@ -2,7 +2,7 @@ import pymysql
 
 
 def test_mysql_available():
-    conn = pymysql.connect(host='localhost',
+    conn = pymysql.connect(host='127.0.0.1',
                     port=3306,
                     user='test_user',
                     password='test_password')
