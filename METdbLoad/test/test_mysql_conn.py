7fe4746333729ede498331ff09c0edb23164d9f2
John Sharples <john.sharples@bom.gov.au>
2024-09-03 20:10:09 +0000

    fix password

--- METdbLoad/test/test_mysql_conn.py
+++ METdbLoad/test/test_mysql_conn.py
@@ -3,8 +3,9 @@ import pymysql
 
 def test_mysql_available():
     conn = pymysql.connect(host='localhost',
-                       user='test',
-                       password='test')
+                    port=3306,
+                    user='test_user',
+                    password='test_password')
     
     conn.cursor().execute("CREATE DATABASE mv_test")
     
